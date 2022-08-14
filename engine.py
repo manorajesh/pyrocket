88064ab80b54176c6f12585f8388da93c0eed33b
manorajesh <mano@raje.sh>
2022-08-13 22:46:03 -0700

    Added color changing to engine.py

--- engine.py
+++ engine.py
@@ -10,15 +10,15 @@ HALF_HEIGHT = HEIGHT // 2
 class AirParticle(pygame.sprite.Sprite):
     def __init__(self, x, y, velocity=(20, 1)):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((2, 2))
+        self.image = pygame.image.load("particle.png")
         self.rect = self.image.get_rect()
         self.image.fill(BACKGROUND)
         self.pos = np.array([x, y], dtype=np.float64)
         self.vel = np.array(velocity, dtype=np.float64)
-        self.nozzle_rect = nozzle.get_rect()
 
         self.temperature = 3400 # Kelvin
-        pygame.draw.circle(self.image, kelvin_table[self.temperature], (1, 1), 1)
+        self.opacity = 255
+        self.image.fill(kelvin_table[self.temperature])
 
     def update(self):
         collisions = pygame.sprite.spritecollide(self, self.groups()[0], False)
@@ -28,6 +28,7 @@ class AirParticle(pygame.sprite.Sprite):
                 collision.vel = -self.vel
 
         self.vel[1] -= 0.01
+        self.vel += np.random.randint(-1, 2, 2)
         self.pos += self.vel
 
         if self.pos[0] < 0 or self.pos[0] > WIDTH:
@@ -35,8 +36,18 @@ class AirParticle(pygame.sprite.Sprite):
         if self.pos[1] < 0 or self.pos[1] > HEIGHT:
             self.vel[1] *= -1 * 0.90
 
+        self.temperature -= 100
+
         screen.fill(BACKGROUND)
         #screen.blit(nozzle, (100, HALF_HEIGHT-250))
+        try:
+            self.image.fill(kelvin_table[self.temperature])
+        except KeyError:
+            self.temperature = 900
+            self.opacity -= 50
+            self.image.set_alpha(self.opacity)
+            if self.opacity <= 0:
+                self.kill()
         self.rect.center = self.pos
 
 if __name__ == "__main__":
@@ -47,7 +58,9 @@ if __name__ == "__main__":
     particles = pygame.sprite.Group()
     clock = pygame.time.Clock()
     while True:
-        clock.tick(100)
+        clock.tick(60)
+        particles.add(AirParticle(0, np.random.randint(HALF_HEIGHT-30, HALF_HEIGHT+30)))
+        particles.add(AirParticle(0, np.random.randint(HALF_HEIGHT-30, HALF_HEIGHT+30)))
         particles.add(AirParticle(0, np.random.randint(HALF_HEIGHT-30, HALF_HEIGHT+30)))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- engine_cocos.py
+++ engine_cocos.py
@@ -1,14 +1,35 @@
 import cocos
+from cocos.director import director
+import cocos.euclid as eu
 import numpy as np
 from kelvin_table import kelvin_table
 
-class AirParticle():
+BACKGROUND = BLACK = (0, 0, 0)
+DAMPING = 0.50
+SCREEN = WIDTH, HEIGHT = 800, 600
+HALF_HEIGHT = HEIGHT // 2
+
+class AirParticle(cocos.sprite.Sprite):
     def __init__(self, x, y, velocity=(20, 1)):
+        super().__init__("particle.png")
         self.pos = np.array([x, y], dtype=np.float64)
         self.vel = np.array(velocity, dtype=np.float64)
 
         self.temperature = 3400 # Kelvin
-        self.particle = cocos.draw.Circle(x, y, 1, color=kelvin_table[self.temperature])
+        self.position = self.pos
 
-    def update(self):
-        pass
+class MainLayer(cocos.layer.Layer):
+    def __init__(self):
+        super().__init__()
+        self.particles = []
+        
+        for i in range(100):  
+            self.particle = AirParticle(0, np.random.randint(HALF_HEIGHT-30, HALF_HEIGHT+30))
+            self.add(self.particle, i)
+
+if __name__ == "__main__":
+    director.init(width=WIDTH, height=HEIGHT, caption="Air Simulation")
+    main_scene = cocos.scene.Scene()
+    sim_layer = MainLayer()
+    main_scene.add(sim_layer)
+    director.run(main_scene)
